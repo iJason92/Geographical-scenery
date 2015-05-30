045313cd0a16abe63c76583bf56dc9c4a8a36ccd
Jason <iJason@163.com>
2015-05-30 22:48:07 +0800

    Init project

---
+++ Geographical-scenery/Geographical-scenery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geographical-scenery
+//
+//  Created by Jason on 30/5/15.
+//  Copyright (c) 2015 Shepherd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
